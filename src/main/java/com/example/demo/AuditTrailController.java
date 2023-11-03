5519b00219e80401a988fc64538d7bb16a67e014
Julien Jouvent-Hallé <julien.halle@dental-wings.com>
2023-11-03 08:24:38 -0400

    Cleanup AuditTrailController.java

--- src/main/java/com/example/demo/AuditTrailController.java
+++ src/main/java/com/example/demo/AuditTrailController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuditTrailController {
 	Logger logger = LogManager.getLogger(AuditTrailController.class);
-    Marker autditMarker = MarkerManager.getMarker("AuditTrail");
+	Marker autditMarker = MarkerManager.getMarker("AuditTrail");
 
 	@GetMapping("/trail")
 	public String index() {
-        logger.info(autditMarker, "1332 call to /trail added to the audit trail");
-        logger.info("1332 /trail called !");
-		return "1332 /trail called !";
+        logger.info(autditMarker, "call to /trail added to the audit trail");
+        logger.info("/trail called !");
+	return "Hello from /trail !";
 	}
 
 }
